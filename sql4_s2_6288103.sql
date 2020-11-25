916b6d1f6d42c67103b189fb50fcb994ea44d0a6
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-11-25 23:48:15 +0700

    Add files via upload

---
+++ sql4_s2_6288103.sql
@@ -0,0 +1,49 @@
+/**
+- Nopparat Pengsuk
+- Section 2
+- 6288103
+*/
+# Q1
+SELECT class.CLASS_CODE, course.COURSE_NAME, p.EMP_FNAME, p.EMP_LNAME, COUNT(*) AS "NUM_STU" FROM class
+JOIN course ON course.COURSE_CODE = class.COURSE_CODE
+JOIN professor AS p ON p.EMP_NUM = class.EMP_NUM
+JOIN enroll AS e ON e.CLASS_CODE = class.CLASS_CODE
+where class.CLASS_SECTION = 3
+GROUP BY class.CLASS_CODE;
+
+#Q2
+SELECT class.class_code, course.COURSE_NAME FROM class
+JOIN course ON class.COURSE_CODE = course.COURSE_CODE
+WHERE CLASS_CODE NOT IN (SELECT CLASS_CODE FROM enroll);
+
+#Q3
+SELECT student.STU_FNAME AS "FIRST NAME", student.STU_LNAME AS "LAST NAME", student.STU_NUM AS "ID" FROM student
+WHERE student.STU_FNAME LIKE "C%"
+UNION -- <<<<<<<<<<<<<<<< shopping mall?
+SELECT p.EMP_FNAME, p.EMP_LNAME, p.EMP_NUM FROM professor p
+WHERE p.EMP_FNAME LIKE "C%";
+
+#Q4
+SELECT class.COURSE_CODE, class.CLASS_SECTION FROM class
+WHERE class.COURSE_CODE NOT IN (SELECT COURSE_CODE FROM class 
+								WHERE class_section = 2)
+ORDER BY class.COURSE_CODE ASC;
+
+#Q5
+SELECT student.STU_FNAME FROM student
+WHERE student.STU_FNAME IN (SELECT EMP_FNAME FROM tinycollege3.professor);
+
+#Q6
+SELECT student.STU_FNAME, student.STU_LNAME, student.STU_GPA FROM student
+WHERE student.STU_GPA > (SELECT avg(STU_GPA) FROM tinycollege3.student
+						where dept_code = "MATH");
+                        
+#Q7
+CREATE VIEW vw_topstudents 
+AS
+SELECT s.STU_NUM, MAX(STU_GPA), d.DEPT_CODE FROM student AS s
+JOIN department AS d ON d.DEPT_CODE = s.DEPT_CODE
+GROUP BY d.DEPT_CODE;
+
+SELECT * FROM vw_topstudents;
+
