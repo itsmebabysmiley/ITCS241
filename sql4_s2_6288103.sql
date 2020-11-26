855cf229bdf82f1eafea8dfeccbcff7ab7de0354
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-11-26 15:11:29 +0700

    Update sql4_s2_6288103.sql

--- sql4_s2_6288103.sql
+++ sql4_s2_6288103.sql
@@ -44,7 +44,8 @@ AS
 SELECT s.STU_NUM, s.STU_FNAME, s.STU_LNAME, s.STU_GPA ,s.DEPT_CODE FROM student AS s
 WHERE s.STU_GPA =  any  (select Max(stu_gpa) as STU_GPA from student 
 						 group by DEPT_CODE); 
-						 -- where student.DEPT_CODE = s.DEPT_CODE);
+# recommed this one
+-- WHERE s.STU_GPA = (select Max(stu_gpa) as STU_GPA from student AS s2 where s2.DEPT_CODE = s.DEPT_CODE ); 
 
 SELECT * FROM vw_topstudents;
 						 
